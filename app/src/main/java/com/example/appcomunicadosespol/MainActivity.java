19e8801c9535b60b764a3083fbe81925e9ef6dcd
GustavoAL2004 <guadalme@espol.edu.ec>
2025-08-08 20:31:54 -0500

    iniciar sesion avance

--- app/src/main/java/com/example/appcomunicadosespol/MainActivity.java
+++ app/src/main/java/com/example/appcomunicadosespol/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.appcomunicadosespol;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
@@ -10,11 +11,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import android.view.View;
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
     private LinearLayout linearLayout_IniciarSesion;
     private EditText editText_Usuario;
     private EditText editText_contraseña;
-
+    private Button button_IniciarSesion;
 
 
     @Override
@@ -27,5 +39,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        linearLayout_IniciarSesion=findViewById(R.id.linearLayout_IniciarSesion);
+        editText_Usuario=findViewById(R.id.editText_Usuario);
+        editText_contraseña=findViewById(R.id.editText_contraseña);
+        button_IniciarSesion=findViewById(R.id.button_IniciarSesion);
+
+    }
+
+    public void IniciarSesion(View view){
+        String usuario=editText_Usuario.getText().toString();
+        String contrasenia=editText_contraseña.getText().toString();
+
+        try (InputStream is = getAssets().open("usuarios.txt");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                String[] listado=linea.split(";");
+                String cred_usuario=listado[0];
+                String cred_contasenia=listado[1];
+                reader.readLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
