05d7946406bc1edcef3cbfda5af0a89d679c681f
GustavoAL2004 <guadalme@espol.edu.ec>
2025-08-10 22:15:10 -0500

    Cambios en publicar comunicado falta exribir el archivo comunicados.txt

--- app/src/main/java/com/example/appcomunicadosespol/PublicarComunicadosActivity.java
+++ app/src/main/java/com/example/appcomunicadosespol/PublicarComunicadosActivity.java
@@ -1,6 +1,7 @@
 package com.example.appcomunicadosespol;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -17,14 +18,19 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Calendar;
 
@@ -44,6 +50,15 @@ public class PublicarComunicadosActivity extends AppCompatActivity {
     private Button botonAdjuntarImagen;
     private Button botonPublicar;
     private Button botonCancelar;
+
+    private LinearLayout layoutAnuncio;
+    private LinearLayout layoutEvento1;
+    private LinearLayout layoutEvento2;
+
+    private ImageView imagenSeleccionada;
+    private static final int REQUEST_IMAGE_PICK = 100;
+    private ActivityResultLauncher<Intent> imagePickerLauncher;
+    private Uri imageUriSeleccionada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,8 +96,67 @@ public class PublicarComunicadosActivity extends AppCompatActivity {
         checkBoxAdministrativo=findViewById(R.id.ckBoxAdministrativo);
         titulo=findViewById(R.id.EditTextTitulo);
         lugar=findViewById(R.id.editTextLugar);
+        layoutAnuncio = findViewById(R.id.LinearLayout_Anuncio);
+        layoutEvento1 = findViewById(R.id.LinearLayout_Evento_pt1);
+        layoutEvento2 = findViewById(R.id.LinearLayout_Evento_pt2);
+        imagenSeleccionada = findViewById(R.id.imagenSeleccionada);
 
 
+        imagePickerLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+
+                    // manejar aquí la imagen seleccionada
+                    if (result.getResultCode() == RESULT_OK && result.getData() != null) {// Se asegura de que la actividad terminó correctamente (RESULT_OK) y que hay datos disponibles (la imagen seleccionada)
+                        Uri originalUri = result.getData().getData(); // Obtiene la URI original de la imagen que el usuario seleccionó en la galería.
+                        try {
+                            InputStream inputStream = getContentResolver().openInputStream(originalUri);//Abre un InputStream para leer los datos binarios de la imagen desde la URI.
+                            String nombreArchivo = "img_" + System.currentTimeMillis() + ".jpg";//Crea un nombre único para la imagen usando la hora actual (en milisegundos).
+                            File file = new File(getFilesDir(), nombreArchivo);//Crea un archivo dentro del almacenamiento interno de la app.
+                            FileOutputStream outputStream = new FileOutputStream(file);//Abre un OutputStream para escribir en el archivo.
+                            byte[] buffer = new byte[1024];//Lee la imagen por partes (en bloques de 1024 bytes) y la escribe en el archivo local. Esto permite copiar imágenes de cualquier tamaño.
+                            int length;
+                            while ((length = inputStream.read(buffer)) > 0) {
+                                outputStream.write(buffer, 0, length);
+                            }
+                            inputStream.close();
+                            outputStream.close();
+                            imageUriSeleccionada = Uri.fromFile(file);//Obtiene una URI segura para la imagen local
+                            imagenSeleccionada.setImageURI(imageUriSeleccionada); // y luego la muestra en el ImageView
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            Toast.makeText(this, "Error al copiar imagen", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        botonAdjuntarImagen=findViewById(R.id.buttonAdjuntarImagen);
+
+        botonAdjuntarImagen.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                imagePickerLauncher.launch(intent);
+
+            }
+        });
+
+        radiogrupo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.radioButtonEsAnuncio) {
+                    // Si el usuario selecciona Anuncio
+                    layoutAnuncio.setVisibility(View.VISIBLE);
+                    layoutEvento1.setVisibility(View.GONE);
+                    layoutEvento2.setVisibility(View.GONE);
+                } else if (checkedId == R.id.radioButtonEsEvento) {
+                    // Si el usuario selecciona Evento
+                    layoutAnuncio.setVisibility(View.GONE);
+                    layoutEvento1.setVisibility(View.VISIBLE);
+                    layoutEvento2.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
 
         fecha=findViewById(R.id.editTextFecha);
@@ -127,10 +201,11 @@ public class PublicarComunicadosActivity extends AppCompatActivity {
         lugar.setText("");
         fecha.setText("");
         radiogrupo.clearCheck();
+        imagenSeleccionada.setImageURI(null);
 
 
     }
-    private void AdjuntarImagen(View view){
+    private void AdjuntarImagen (View view){
         try (FileInputStream fis = openFileInput("comunicado.txt");
              BufferedReader reader = new BufferedReader(new InputStreamReader(fis))) {
 
@@ -152,7 +227,7 @@ public class PublicarComunicadosActivity extends AppCompatActivity {
                 imgv.setImageURI(uri);
 
                 imgv.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 600));
-                // You would need to add this to a parent view to display it
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -165,6 +240,8 @@ public class PublicarComunicadosActivity extends AppCompatActivity {
         String lugarTexto = lugar.getText().toString().trim();
         String fechaTexto = fecha.getText().toString().trim();
         String descripcionTexto = descripcion.getText().toString().trim();
+        int tipoSeleccionadoId = radiogrupo.getCheckedRadioButtonId();
+
 
         if (radiogrupo.getCheckedRadioButtonId() == -1) {
             try {
@@ -210,22 +287,35 @@ public class PublicarComunicadosActivity extends AppCompatActivity {
             return;
         }
     }
-        if (lugarTexto.isEmpty()){
-            try {
-                throw new datosIncompletosException("LLene el campo del lugar");
-            }catch (datosIncompletosException e){
-                Toast.makeText(PublicarComunicadosActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
-                return;
+        if (tipoSeleccionadoId == R.id.radioButtonEsAnuncio) {
+            if (spinnerNivelDeUrgencia.getSelectedItemPosition() == -1){
+                try {
+                    throw new datosIncompletosException("Selecciona al menos un nivel de urgencia");
+                }catch (datosIncompletosException e){
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
             }
         }
-        if (fechaTexto.isEmpty()){
-            try {
-                throw new datosIncompletosException("LLene el campo de la fecha");
-            }catch (datosIncompletosException e){
-                Toast.makeText(PublicarComunicadosActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
-                return;
+        if (tipoSeleccionadoId == R.id.radioButtonEsEvento) {
+            if (lugarTexto.isEmpty()){
+                try {
+                    throw new datosIncompletosException("Llena el campo del lugar");
+                }catch (datosIncompletosException e){
+                    Toast.makeText(PublicarComunicadosActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+            if (fechaTexto.isEmpty()){
+                try {
+                    throw new datosIncompletosException("Llena el campo de la fecha");
+                }catch (datosIncompletosException e){
+                    Toast.makeText(PublicarComunicadosActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                    return;
+                }
             }
         }
+
         if (descripcionTexto.isEmpty()){
             try {
                 throw new datosIncompletosException("LLene el campo de la descripcion");
@@ -235,10 +325,16 @@ public class PublicarComunicadosActivity extends AppCompatActivity {
             }
         }
 
+        if (imageUriSeleccionada==null){
+            try {
+                throw new datosIncompletosException("Adjunte una imagen");
+            }catch (datosIncompletosException e){
+                Toast.makeText(PublicarComunicadosActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
 
 
 
 
-}
-}
-
+        }}
